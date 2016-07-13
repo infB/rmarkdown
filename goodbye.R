2a982b0a15bd7d98a0c907f2e1aca0238d3fb801
tzimmerm <tobias.zimmermann@computational.bio.uni-giessen.de>
2016-07-13 14:46:08 +0200

    nice rscripts to rmd

---
+++ goodbye.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "HelloWorld"
+#' author: "m"
+#' date: "13 Juli 2016"
+#' output: github_document
+#' ---
+
+#+ setup, include=FALSE
+knitr::opts_chunk$set(echo = TRUE)
+
+
+#' Now I'm flying on my own.
+# this is data from gapminder.org
+library(gapminder)
+summary(gapminder)
+
